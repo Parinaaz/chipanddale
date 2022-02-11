e666aaaa54eba45913acd4dc131ed162fdd13682
unknown <parinaaz.cobla@gmail.com>
2022-02-11 22:27:27 +0530

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app=express();
 
-var port_no = server.listen(process.env.PORT || 3000);
+
 
 app.get('/', (req,res)=>
 {
@@ -9,4 +9,4 @@ app.get('/', (req,res)=>
     res.json({'message' : 'world'});
 })
 
-app.listen(port_no);
+app.listen(process.env.PORT || 3000);
