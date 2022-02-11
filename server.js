0d5263ce98da85b7854a06ef47f2fadf3a206fcf
unknown <parinaaz.cobla@gmail.com>
2022-02-11 19:19:32 +0530

    Commit node

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("JS file");
+var express = require('express');
+var app=express();
+
+var PORT = 3000 || process.env.PORT;
+
+app.get('/articles', (req,res)=>
+{
+
+    res.json({'message' : 'world'});
+})
+
+app.listen(PORT);
