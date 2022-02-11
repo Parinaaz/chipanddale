8fa76f3e0b3115d9f8411690c0d0fea133ca831e
unknown <parinaaz.cobla@gmail.com>
2022-02-11 22:23:51 +0530

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app=express();
 
-var port_no = 3000 || process.env.PORT;
+var port_no = server.listen(process.env.PORT || 3000);
 
 app.get('/', (req,res)=>
 {
