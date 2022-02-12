994efccb35e80da5250900a1b8bd00a278e8f44f
unknown <parinaaz.cobla@gmail.com>
2022-02-12 06:50:59 +0530

    branch

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express');
+
 var app=express();
+var bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req,res)=>
 {
 
-    res.json({'message' : 'world'});
+    res.sendFile(__dirname + '/index.html');
 })
+
+app.post('/article', (req, res)=>{
+    res.send("Hi you successfully posted!!");
+    console.log(req.body);
+})
+
 
 app.listen(process.env.PORT || 3000);
